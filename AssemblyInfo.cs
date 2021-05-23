1a88932705fa34c925c4074dc21e3b194061e046
Greydus <caleblionscat@hotmail.com>
2021-05-23 15:53:50 -0600

    Version change

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly:AssemblyVersionAttribute("1.0.*")]
+[assembly:AssemblyVersionAttribute("1.1.*")]
 [assembly:AssemblyFileVersionAttribute("1.3.3.7")]
 [assembly:AssemblyDescriptionAttribute("Network testing tool")]
 [assembly:AssemblyTitleAttribute("Rabbit")]
