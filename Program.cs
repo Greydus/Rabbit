03a0dc428526438d17db2df5b6c1950d9f7a6287
Greydus <caleblionscat@hotmail.com>
2021-06-19 21:08:21 -0600

    Code cleanup

--- Program.cs
+++ Program.cs
@@ -7,62 +7,41 @@ namespace Rabbit
 {
 	class Program
 	{
-		//static StringBuilder _headersTxt = new StringBuilder();
-		//static String _bodyTxt = String.Empty;
-
 		static void Main(String[] args)
 		{
 			try
 			{
-				Uri _uri = new Uri(args[0]);
-				HttpWebRequest _hwreq = WebRequest.CreateHttp(_uri);
-				_hwreq.UserAgent = "Rabbit/1.3.3.7";
+				var _headers = new StringBuilder();
+				var _body = String.Empty;
 
-				//WebResponse _wres = _hwreq.GetResponse();
-				StringBuilder _headersTxt = new StringBuilder();
-				String _bodyTxt = String.Empty;
-				using (WebResponse _wres = _hwreq.GetResponse())
+				var _uri = new Uri(args[0]);
+				var _wreq = WebRequest.CreateHttp(_uri);
+
+				_wreq.UserAgent = "Rabbit/1.3.3.7";
+
+				using (var _wres = _wreq.GetResponse())
 				{
-					//WebHeaderCollection _whc = _webres.Headers;
-					using (StringWriter _headers = new StringWriter(_headersTxt))
+					using (var __headers = new StringWriter(_headers))
 					{
-						WebHeaderCollection _whc = _wres.Headers;
-						AsyncWriteHeadersToString(_headers, _whc);
-						/*for (int i = 0; i < _whc.Count; ++i)
-						{
-							_headers.WriteLine(_whc.GetKey(i) + ": " + _whc.Get(i));
-							//Console.WriteLine(_whc.GetKey(i) + ": " + _whc.Get(i));
-						}*/
+						var ___headers = _wres.Headers;
+
+						AsyncWriteHeadersToString(__headers, ___headers);
 					}
-					using (Stream _wrs = _wres.GetResponseStream())
+
+					using (var __wres = _wres.GetResponseStream())
 					{
-						using (StreamReader _body = new StreamReader(_wrs))
+						using (var __body = new StreamReader(_wres))
 						{
-							_bodyTxt = _body.ReadToEnd();
+							_body = __body.ReadToEnd();
 						}
 					}
 				}
-				Console.WriteLine(_headersTxt);
-				Console.WriteLine(_bodyTxt);
+
+				Console.WriteLine(_headers);
+				Console.WriteLine(_body);
 			}
 			catch (WebException e)
 			{
-				/*StringBuilder _headersTxt = new StringBuilder();
-				String _bodyTxt = String.Empty;
-				using (StringWriter _headers = new StringWriter(_headersTxt))
-				{
-					WebHeaderCollection _whc = e.Response.Headers;
-					AsyncWriteHeadersToString(_headers, _whc);
-				}
-				using (Stream _wrs = e.Response.GetResponseStream())
-				{
-					using (StreamReader _body = new StreamReader(_wrs))
-					{
-						_bodyTxt = _body.ReadToEnd();
-					}
-				}
-				Console.WriteLine(_headersTxt);
-				Console.WriteLine(_bodyTxt);*/
 				Console.WriteLine(e.Message);
 			}
 		}
